fc64646d3f7af0dc1ee07872cb50a32bed829c4b
Angelina Yona Listianingsih <43510088+angelinayl@users.noreply.github.com>
2018-09-30 16:07:43 +0700

    Ini adalah form awal
    Form awal adalah halaman login yang terdiri dari kolom Username dan Password

--- formawal.php
+++ formawal.php
@@ -1,5 +1,38 @@
-<?php
+<h6> Angelina Yona Listianingsih </h6>
 
+<?php 
+session_start();
+error_reporting(0);
+		$user = array(
+				"user" => "Angelina",
+				"pass" => "12345"
+			);
 
+if(isset($_POST['submit'])){
+	if($_POST['username']==$user['user']&&$_POST['password']==$user['pass']) {
 
-?>
+		$_SESSION["username"] = $_POST['username'];
+		echo "Anda Berhasil Log In, $_POST[username]!";
+
+	} else {
+		display_login_form();
+		echo '<p> Username Atau Password Salah</p>';
+	}
+}
+else {
+	display_login_form();
+}
+function display_login_form(){ ?>
+	<form action ="proses.php" method='POST'>
+		<h1><center> Form Log In </center></h1>
+		<center><label for="username"> Username : </label></center>
+		<center><input type="text" name="username" id="username"></center>
+		<br><br>
+		<center><label for="password"> Password : </label></center>
+		<center><input type="password" name="password" id="password"></center>
+		<br><br>
+		<center><input type="submit" name="submit" value="Log In"></center>
+	</form>
+<?php 
+}
+ ?>
